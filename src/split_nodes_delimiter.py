b5f8e9892c58db30ebff61b9ec058199bde44062
JonEast87 <j.d.east@icloud.com>
2024-08-15 20:56:38 -0700

    slight updates to split_nodes, working on getting image test to pass

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -2,15 +2,17 @@ from textnode import TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
 	new_nodes_list = list()
-	old_nodes_split = old_nodes[0].text.split(f"{delimiter}")
-	if text_type == "link" or text_type == "image":
-		new_nodes_list.extend(old_nodes)
-	else:
-		return_node = iterate_nodes(old_nodes_split, text_type)
-		new_nodes_list.extend(return_node)
-		return new_nodes_list
+	for old_node in old_nodes:
+		if old_node.text_type == "text":
+			new_nodes_list.append(old_node)
+			return new_nodes_list
+		else:
+			old_nodes_split = old_node.text.split(f"{delimiter}")
+			return_node = iterate_nodes(old_nodes_split, text_type)
+			new_nodes_list.extend(return_node)
+			return new_nodes_list
 
-	raise Exception("Unsupported text_type!")
+		raise Exception("Unsupported text_type!")
 
 # Helper function to check for text types versus styled types
 def iterate_nodes(old_nodes_split, text_type):
--- src/test_split_node_delimiter.py
+++ src/test_split_node_delimiter.py
@@ -3,16 +3,52 @@ import unittest
 from split_nodes_delimiter import split_nodes_delimiter
 from textnode import TextNode
 
-class TestTextNode(unittest.TestCase):
-	def test_eq(self):
+class TestTextNodeToHTMLNode(unittest.TestCase):
+	def test_code(self):
 		node = TextNode("This is text with a `code block` word", "code")
-		new_nodes = split_nodes_delimiter([node], "`", "code")
+		html_node = split_nodes_delimiter([node], "`", "code")
 		expected_results = [
     		TextNode("This is text with a ", "text", None),
     		TextNode("code block", "code", None),
     		TextNode(" word", "text", None),
 		]
 		self.assertEqual(new_nodes, expected_results)
+
+	def test_bold(self):
+		node = TextNode("This is text with a **bolded** word", "bold")
+		html_node = split_nodes_delimiter([node], "**", "bold")
+		expected_results = [
+    		TextNode("This is text with a ", "text", None),
+    		TextNode("bolded", "bold", None),
+    		TextNode(" word", "text", None),
+		]
+		self.assertEqual(new_nodes, expected_results)
+
+	def test_text(self):
+		node = TextNode("This is text", "text")
+		html_node = split_nodes_delimiter([node], "", "text")
+		expected_results = [
+    		TextNode("This is text", "text", None)
+		]
+		self.assertEqual(new_nodes, expected_results)
+
+	def test_text(self):
+		node = TextNode("This is text", "link")
+		html_node = split_nodes_delimiter([node], "", "text")
+		expected_results = [
+    		TextNode("This is text", "text", None)
+		]
+		self.assertEqual(new_nodes, expected_results)
+
+	def test_image(self):
+		node = TextNode("This is an image", "image", "https://www.boot.dev")
+		html_node = text_node_to_html_node(node)
+		self.assertEqual(html_node.tag, "img")
+		self.assertEqual(html_node.value, "")
+		self.assertEqual(
+			html_node.props,
+			{"src": "https://www.boot.dev", "alt": "This is an image"},
+		)
 
 if __name__ == "__main__":
 	unittest.main()
