41e699996b4cc786bc1172330d15ca9993fb0164
JonEast87 <j.d.east@icloud.com>
2024-08-17 08:21:21 -0700

    slight refactoring of inline_markdown.py and renamed it. textnode has been consolidated for size. testing has been updated with better testing

---
+++ src/inline_markdown.py
@@ -0,0 +1,34 @@
+from textnode import (
+	TextNode,
+	text_type_text,
+	text_type_bold,
+	text_type_italic,
+	text_type_code,
+)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+	new_nodes_list = list()
+	for old_node in old_nodes:
+		if old_node.text_type != text_type_text:
+			new_nodes_list.append(old_node)
+			continue
+
+		text_list = list()
+		old_nodes_split = old_node.text.split(delimiter)
+
+		if len(old_nodes_split) % 2 == 0:
+			raise ValueError("Invalid markdown, format not closed.")
+		# Merged the helper here to consolidate
+		for node in old_nodes_split:
+			if node.endswith(" "):
+				text_list.append(TextNode(node, text_type_text))
+			elif node.startswith(" "):
+				text_list.append(TextNode(node, text_type_text))
+			# Added to ensure end any styles that end the sentence are not including a blank space
+			elif node == "":
+				continue
+			else:
+				text_list.append(TextNode(node, text_type))
+
+		new_nodes_list.extend(text_list)
+	return new_nodes_list
