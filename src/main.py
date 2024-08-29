c7878880d617642ce7ac602bd64af370d37ae686
JonEast87 <j.d.east@icloud.com>
2024-08-28 21:03:16 -0700

    working on main.py to handle the clearing of ./public and copying of ./static to ./public for the ssg. will rework it to have it be recursive

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
-from textnode import *
+import os, shutil
 
 def main():
-	dummy = TextNode("This is a text node", "bold", "https://www.boot.dev")
-	print(dummy)
+	destination = "./public/"
+	source = "./static/"
+
+	delete_files = os.listdir(destination)
+	copy_files = os.listdir(source)
+	# for file in delete_files:
+	# 	if os.path.exists(destination + file):
+	# 		os.remove(destination + file)
+	# 	if os.path.isdir(destination + file):
+	# 		shutil.rmtree(destination + file)
+
+	for copy_file in copy_files:
+		if os.path.exists(source + copy_file) and os.path.isfile(source + copy_file):
+			shutil.copy(source + copy_file, destination)
+		if os.path.isdir(source + copy_file):
+			try:
+				shutil.copytree(source + copy_file, destination + copy_file)
+			except:
+				raise Exception(source + copy_file)
+		else:
+			# For debugging
+			print(destination + copy_file)
 
 main()
