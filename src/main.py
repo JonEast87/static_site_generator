83f88e97195e1f6ee787490b223af1fe90a6107d
JonEast87 <j.d.east@icloud.com>
2024-08-28 21:05:38 -0700

    minor tweaks to main.py

--- src/main.py
+++ src/main.py
@@ -6,17 +6,18 @@ def main():
 
 	delete_files = os.listdir(destination)
 	copy_files = os.listdir(source)
-	# for file in delete_files:
-	# 	if os.path.exists(destination + file):
-	# 		os.remove(destination + file)
-	# 	if os.path.isdir(destination + file):
-	# 		shutil.rmtree(destination + file)
+	for file in delete_files:
+		if os.path.exists(destination + file):
+			os.remove(destination + file)
+		if os.path.isdir(destination + file):
+			shutil.rmtree(destination + file)
 
 	for copy_file in copy_files:
 		if os.path.exists(source + copy_file) and os.path.isfile(source + copy_file):
 			shutil.copy(source + copy_file, destination)
 		if os.path.isdir(source + copy_file):
 			try:
+				# Will replace this to do it recursively, since that is what the assignment calls for
 				shutil.copytree(source + copy_file, destination + copy_file)
 			except:
 				raise Exception(source + copy_file)
