4f4540b7296503a01cb8f18e223127a8b3d3cc07
JonEast87 <j.d.east@icloud.com>
2024-08-29 19:19:58 -0700

    standardized both for loops before moving on and making the copy loop into a recurvise function. although I kind of want to move on.

--- src/main.py
+++ src/main.py
@@ -6,23 +6,20 @@ def main():
 
 	delete_files = os.listdir(destination)
 	copy_files = os.listdir(source)
+
 	for file in delete_files:
 		if os.path.exists(destination + file):
-			os.remove(destination + file)
-		if os.path.isdir(destination + file):
-			shutil.rmtree(destination + file)
+			if os.path.isfile(destination + file):
+				os.remove(destination + file)
+			if os.path.isdir(destination + file):
+				shutil.rmtree(destination + file)
 
 	for copy_file in copy_files:
-		if os.path.exists(source + copy_file) and os.path.isfile(source + copy_file):
-			shutil.copy(source + copy_file, destination)
-		if os.path.isdir(source + copy_file):
-			try:
+		if os.path.exists(source + copy_file):
+			if os.path.isfile(source + copy_file):
+				shutil.copy(source + copy_file, destination)
+			if os.path.isdir(source + copy_file):
 				# Will replace this to do it recursively, since that is what the assignment calls for
 				shutil.copytree(source + copy_file, destination + copy_file)
-			except:
-				raise Exception(source + copy_file)
-		else:
-			# For debugging
-			print(destination + copy_file)
 
 main()
