02e49d9d926c3e0c6b37a641363b4774c9825833
JonEast87 <j.d.east@icloud.com>
2024-08-31 09:06:41 -0700

    seperate function calls, delete will be non recursive. copy will be refactored to recursion according to the assignment needs

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
 import os, shutil
 
-def main():
-	destination = "./public/"
-	source = "./static/"
-
-	delete_files = os.listdir(destination)
-	copy_files = os.listdir(source)
-
+def delete(dst):
+	delete_files = os.listdir(dst)
 	for file in delete_files:
-		if os.path.exists(destination + file):
-			if os.path.isfile(destination + file):
-				os.remove(destination + file)
-			if os.path.isdir(destination + file):
-				shutil.rmtree(destination + file)
+		if os.path.exists(dst + file):
+			if os.path.isfile(dst + file):
+				os.remove(dst + file)
+			if os.path.isdir(dst + file):
+				shutil.rmtree(dst + file)
 
+def copy(src, dst):
+	copy_files = os.listdir(src)
 	for copy_file in copy_files:
-		if os.path.exists(source + copy_file):
-			if os.path.isfile(source + copy_file):
-				shutil.copy(source + copy_file, destination)
-			if os.path.isdir(source + copy_file):
+		if os.path.exists(src + copy_file):
+			if os.path.isfile(src + copy_file):
+				shutil.copy(src + copy_file, dst)
+			if os.path.isdir(src + copy_file):
 				# Will replace this to do it recursively, since that is what the assignment calls for
-				shutil.copytree(source + copy_file, destination + copy_file)
+				shutil.copytree(src + copy_file, dst + copy_file)
+
+def main():
+	source = "./static/"
+	destination = "./public/"
+	delete(destination)
+	copy(source, destination)
 
 main()
